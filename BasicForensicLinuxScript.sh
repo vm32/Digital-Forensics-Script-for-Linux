4360e43cbb238ba91e8eaca7e36bfc800c72cbde
Abdullah <abdullah@linux.com>
2023-12-03 19:26:17 +0300

    Update BasicForensicLinuxScript.sh

--- BasicForensicLinuxScript.sh
+++ BasicForensicLinuxScript.sh
@@ -15,40 +15,40 @@ write_output() {
 }
 
 # System Information Extraction
-write_output "uptime -p" "days_On.txt"
-write_output "uptime -s" "start_time.txt"
-write_output "date" "current_date.txt"
-write_output "date +%s" "current_timestamp.txt"
-write_output "hwclock -r" "hardware_clock.txt"
+write_output "uptime -p" "system_uptime.txt"
+write_output "uptime -s" "system_startup_time.txt"
+write_output "date" "current_system_date.txt"
+write_output "date +%s" "current_unix_timestamp.txt"
+write_output "hwclock -r" "hardware_clock_readout.txt"
 
 # Operating System Installation Date
-write_output "df -P /" "root_fs_info.txt"
-write_output "ls -l /var/log/installer" "installer_log.txt"
-write_output "tune2fs -l /dev/sda1" "filesystem_info.txt" # Assuming /dev/sda1 is the root partition
+write_output "df -P /" "root_filesystem_info.txt"
+write_output "ls -l /var/log/installer" "os_installer_log.txt"
+write_output "tune2fs -l /dev/sda1" "root_partition_filesystem_details.txt" # Assuming /dev/sda1 is the root partition
 
 # Network Information
-write_output "ifconfig" "network_info.txt"
-write_output "ip addr" "ip_addresses.txt"
-write_output "netstat -i" "net_interfaces.txt"
+write_output "ifconfig" "network_configuration.txt"
+write_output "ip addr" "ip_address_info.txt"
+write_output "netstat -i" "network_interfaces.txt"
 
 # Installed Programs
-write_output "rpm -qa" "installed_packages_rpm.txt"
-write_output "apt list --installed" "installed_packages_apt.txt"
+write_output "rpm -qa" "rpm_installed_packages.txt"
+write_output "apt list --installed" "apt_installed_packages.txt"
 
 # Hardware Information
-write_output "lspci" "pci_devices.txt"
-write_output "lshw -short" "hardware_summary.txt"
-write_output "dmidecode" "dmi_info.txt"
+write_output "lspci" "pci_device_list.txt"
+write_output "lshw -short" "hardware_summary_report.txt"
+write_output "dmidecode" "dmi_bios_info.txt"
 
 # System Logs and Usage
-write_output "journalctl" "system_journal.txt"
-write_output "ls -lah /var/log/" "var_log_listing.txt"
+write_output "journalctl" "system_journal_logs.txt"
+write_output "ls -lah /var/log/" "var_log_directory_listing.txt"
 
 # User Specific Data
 for user_home in /home/*; do
     username=$(basename "$user_home")
-    write_output "cat $user_home/.local/share/recently-used.xbel" "recently_used_$username.txt"
-    write_output "cat $user_home/.bash_history" "bash_history_$username.txt"
+    write_output "cat $user_home/.local/share/recently-used.xbel" "recently_used_files_$username.txt"
+    write_output "cat $user_home/.bash_history" "bash_command_history_$username.txt"
 done
 
 # Compress User Data
