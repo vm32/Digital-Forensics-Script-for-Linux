448075489b0ea067acf3ec833d7f7e22ad93e652
Abdullah <abdullah@linux.com>
2023-12-08 18:26:53 +0300

    new deploy

--- BasicForensicLinuxScript.sh
+++ BasicForensicLinuxScript.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-
-# Define the output directory
 output_dir="/tmp/ExtractedInfo"
 mkdir -p "$output_dir/hash"
+logfile="$output_dir/forensics_log.txt"
 
-# Assign execution permissions to the forensic script, if needed
-# chmod +x /path/to/BasicForensicLinuxScript.sh
-
-# Function to write command output to a file in the output directory
 write_output() {
     command=$1
     filename=$2
-    $command > "$output_dir/$filename"
+    if $command > "$output_dir/$filename"; then
+        echo "Successfully executed: $command" >> "$logfile"
+    else
+        echo "Failed to execute: $command" >> "$logfile"
+    fi
 }
+
+perform_memory_dump() {
+    memory_dump_cmd="dump -w $output_dir/memory_dump.img"
+    write_output "$memory_dump_cmd" "memory_dump.txt"
+}
+
+echo "Forensic data extraction started at $(date)" > "$logfile"
 
 # System Information Extraction
 write_output "uptime -p" "system_uptime.txt"
@@ -24,7 +30,7 @@ write_output "hwclock -r" "hardware_clock_readout.txt"
 # Operating System Installation Date
 write_output "df -P /" "root_filesystem_info.txt"
 write_output "ls -l /var/log/installer" "os_installer_log.txt"
-write_output "tune2fs -l /dev/sda1" "root_partition_filesystem_details.txt" # Assuming /dev/sda1 is the root partition
+write_output "tune2fs -l /dev/sda1" "root_partition_filesystem_details.txt" # Check for correct root partition
 
 # Network Information
 write_output "ifconfig" "network_configuration.txt"
@@ -51,8 +57,18 @@ for user_home in /home/*; do
     write_output "cat $user_home/.bash_history" "bash_command_history_$username.txt"
 done
 
-# Compress User Data
-tar -czf "$output_dir/user_data.tar.gz" -C "$output_dir" ./*.txt --remove-files
+perform_memory_dump
+write_output "ps aux" "current_processes.txt"
+write_output "lastlog" "user_login_history.txt"
+write_output "crontab -l" "scheduled_cron_jobs.txt"
+write_output "ss -tulnw" "active_network_connections.txt"
 
-# Cleanup and Final Messages
-echo "Data extraction complete. Check the $output_dir directory for output."
+# Secure Output Handling
+tar -czf "$output_dir/user_data.tar.gz" -C "$output_dir" ./*.txt --remove-files
+sha256sum $output_dir/*.txt > "$output_dir/checksums.sha256"
+
+echo "Data extraction complete. Check the $output_dir directory for output." >> "$logfile"
+echo "Forensic data extraction completed at $(date)" >> "$logfile"
+gpg --batch --yes -c --passphrase "YOUR_PASSPHRASE" "$output_dir/user_data.tar.gz"
+
+echo "Data extraction and encryption complete. Check the $output_dir directory for output."
